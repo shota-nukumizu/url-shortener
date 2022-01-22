9216e9ea1d9198c4ff7738853318da011249c330
shota-nukumizu <author2000.1225@gmail.com>
2022-01-23 06:58:15 +0900

    setting JS for shortening URL successfully

--- app.py
+++ app.py
@@ -4,21 +4,20 @@ from flask import Flask, render_template
 
 url = "https://url-shortener-service.p.rapidapi.com/shorten"
 
-payload = "url=https%3A%2F%2Fgoogle.com%2F"
-headers = {
-    'content-type': "application/x-www-form-urlencoded",
-    'x-rapidapi-host': "url-shortener-service.p.rapidapi.com",
-    'x-rapidapi-key': config('API_KEY')
-    }
-
-# response = requests.request("POST", url, data=payload, headers=headers)
-
 # print(response.text)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    payload = "url="
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'x-rapidapi-host': "url-shortener-service.p.rapidapi.com",
+        'x-rapidapi-key': config('API_KEY')
+        }
+
+# response = requests.request("POST", url, data=payload, headers=headers)
     return render_template('index.html')
 
 if __name__ == '__main__':
---
+++ static/index.js
@@ -0,0 +1,29 @@
+const { response } = require("express")
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+
+    let long_url = formData.get('long_url')
+
+    const URL = '/'
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({long_url:long_url})
+    }
+
+    fetch(URL, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => console.log(error))
+
+    console.log('Hello')
+})
