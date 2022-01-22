906951da6fea828c5e49aee77c40b37db3276479
shota-nukumizu <author2000.1225@gmail.com>
2022-01-23 08:56:52 +0900

    create url shorten

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ from flask import Flask, render_template, request
 
 url = "https://url-shortener-service.p.rapidapi.com/shorten"
 
-# payload = "url="
-# headers = {
-#     'content-type': "application/x-www-form-urlencoded",
-#     'x-rapidapi-host': "url-shortener-service.p.rapidapi.com",
-#     'x-rapidapi-key': config('API_KEY')
-#     }
 
-# response = requests.request("POST", url, data=payload, headers=headers)
-
-# print(response.text)
 
 app = Flask(__name__)
 
@@ -24,7 +15,16 @@ def index():
 
         long_url = data.get('long_url')
 
-        print(long_url)
+        payload = f"url={long_url}"
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'x-rapidapi-host': "url-shortener-service.p.rapidapi.com",
+            'x-rapidapi-key': config('API_KEY')
+            }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+
+        print(response.json()['result_url'])
 
     return render_template('index.html')
 
